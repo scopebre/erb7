05c242e85795baa1d4cab763b222247abc78c28c
wscheungc <scopebre@gmail.com>
2025-10-06 12:59:06 +0800

    step 13 : listing page.

--- listings/views.py
+++ listings/views.py
@@ -18,8 +18,10 @@ def listings(request):
     context = {"listings":paged_listings}
     return render(request, 'listings/listings.html',context)
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request,listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    context = {"listing":listing}
+    return render(request, 'listings/listing.html',context)
 
 def search(request):
     return render(request, 'listings/search.html')
